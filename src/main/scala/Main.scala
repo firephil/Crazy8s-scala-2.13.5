e94ecb0168cd04cb941ad0027613a51c2667d96b
firephil <philip@firesoft.gr>
2021-03-12 21:32:31 +0200

    Use Java's secure Random generator from java.security

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,17 @@
 import scala.util.Random
+import java.security.SecureRandom
 
 object Main {
 
+  // Use java's secure random as suggested by codacy quality checker
+  val random = new SecureRandom()
 
   def crazy8(a: Int, b: Int): LazyList[String] = {
     val chars = (33 to 47) ++ (58 to 126)
 
     val list :LazyList[String] = LazyList.range(a,b).map {
       case x if x== 0  => String.valueOf(x)
-      case x if x % 8 == 0 => chars(Random.nextInt(chars.length)).toChar.toString
+      case x if x % 8 == 0 => chars(random.nextInt(chars.length)).toChar.toString
       case x => String.valueOf(x)}
 
     list
@@ -17,7 +20,7 @@ object Main {
   //Golfed inlined version
   // Don't annotate types, use a range and IndexedSeq, Vector, eliminate variable declarations,spaces etc
 
-  def S(a:Int,b:Int)= a to b map(x=>if(x%8==0)Random.nextInt(((33 to 47) ++ (58 to 126)).length).toChar.toString else String.valueOf(x))
+  def S(a:Int,b:Int)= a to b map(x=>if(x%8==0)random.nextInt(((33 to 47) ++ (58 to 126)).length).toChar.toString else String.valueOf(x))
 
 
 
