0dd46c419ece3f2967138f496cd14c6c429bbb80
firephil <philip@firesoft.gr>
2021-03-13 16:59:49 +0200

    Add a solution based on Parallel Vector Collection to test the performance of parallel code.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,20 +1,32 @@
-import scala.util.Random
 import java.security.SecureRandom
+import scala.collection.parallel.immutable.ParVector
+
 
 object Main {
 
   // Use java's secure random as suggested by codacy quality checker
   val random = new SecureRandom()
 
-  def crazy8(a: Int, b: Int): LazyList[String] = {
+  def crazy8(a: Int, b: Int): List[String] = {
     val chars = (33 to 47) ++ (58 to 126)
 
-    val list :LazyList[String] = LazyList.range(a,b).map {
+    val list :List[String] = List.range(a,b).map {
       case x if x== 0  => String.valueOf(x)
       case x if x % 8 == 0 => chars(random.nextInt(chars.length)).toChar.toString
       case x => String.valueOf(x)}
 
     list
+  }
+
+  // Testing the ParVector Performance
+  def crazy8Parallel(a: Int, b: Int): ParVector[String] = {
+    val chars = (33 to 47) ++ (58 to 126)
+
+    val vec :ParVector[String] = ParVector.range(a,b).map {
+      case x if x % 8 == 0 => chars(random.nextInt(chars.length)).toChar.toString
+      case x => String.valueOf(x)}
+
+    vec
   }
 
   //Golfed inlined version
@@ -26,8 +38,7 @@ object Main {
 
 
   def main(args: Array[String]): Unit = {
-
-    println(crazy8(0,20).toList)
-    println(S(0,20))
+    Timer.average(crazy8(1, 100_0000))(10)
+    Timer.average(crazy8Parallel(1,100_0000))(10)
   }
 }
---
+++ src/main/scala/Timer.scala
@@ -0,0 +1,47 @@
+object Timer {
+
+
+  def timePrint[R](f: => R): Unit = {
+
+    println(timeString(f))
+  }
+
+  def time[R](f: => R): Long = {
+    val t0 = System.nanoTime()
+    val r = f
+    val t1 = System.nanoTime()
+
+    val time = (t1 - t0) / 1000
+
+    time
+  }
+
+  def timeString[R](f: => R): String = {
+    val t0 = System.nanoTime()
+    val r = f
+    val t1 = System.nanoTime()
+
+    val nanos = t1 - t0
+
+    val ms = nanos / 1000000d
+
+    val st = ms.toString.slice(0, 5)
+
+    val result = s"time taken $st ms , The result is: $r"
+
+    result
+  }
+
+  def average[R](f: => R)(times: Int = 10): Unit = {
+
+    var result = 0L
+
+    // use for with range
+    for (a <- 1 to times)
+      result = result + time(f)
+
+    //average time taken
+    val average = result / (times * 1000d)
+    println(s"time taken: $average ms ")
+  }
+}
